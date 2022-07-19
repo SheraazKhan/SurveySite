d59d4113d47ce6fcf3ae1c3a58293c99e1a13798
Thiago <tdecasti@my.centennialcollege.ca>
2022-07-19 19:13:40 -0400

    Test Change Commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
- var app = require('./server/config/app');
+ let app = require('./server/config/app');
 var debug = require('debug')('a1:server');
 var http = require('http');
 
